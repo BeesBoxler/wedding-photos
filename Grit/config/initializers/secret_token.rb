138c74c956ff7a79642cda5147b92caae2e47510
Benjamin Boxler <benjaminboxler@gmail.com>
2014-03-30 23:06:29 +0100

    I chose RoR

---
+++ Grit/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Grit::Application.config.secret_token = '1128e9d18712b087d803e61ee12e82ad23dae788d69a9c25a936e884985fb373c51f198570ba956041a520560bf7b6cd013dc241afd4d211baa343144eb0dafa'
