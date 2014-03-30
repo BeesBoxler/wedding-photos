83a2d2889a373a7ea654d124d9a64e0ddbb2ceed
Benjamin Boxler <benjaminboxler@gmail.com>
2014-03-31 00:35:27 +0100

    relationships

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140330231942) do
+ActiveRecord::Schema.define(:version => 20140330232858) do
+
+  create_table "photos", :force => true do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+    t.string   "email"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email"
