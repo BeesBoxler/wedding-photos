1a2f185dbc90297c37c4b0e4121a21441538fa93
Benjamin Boxler <benjaminboxler@gmail.com>
2014-03-31 20:53:46 +0100

    AddImageToPhotos

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,4 +2,12 @@ class Photo < ActiveRecord::Base
   # attr_accessible :title, :body
 
   belongs_to :user
+
+  has_attached_file :image, styles: {
+    small: '400x400>',
+    full:'100%'
+  }
+
+  alidates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+  
 end
