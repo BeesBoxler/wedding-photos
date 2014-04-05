ba51996ea7c3dbc361933bb6b8c22fc4c402b0ec
Benjamin Boxler <benjaminboxler@gmail.com>
2014-04-05 01:57:18 +0100

    Precompile error

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,7 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-c
+
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
   Bundler.require(*Rails.groups(:assets => %w(development test)))
@@ -13,7 +13,7 @@ end
 module Grit
   class Application < Rails::Application
 
-    onfig.assets.initialize_on_precompile = false
+    config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
