51162c75dfb3c3d7999de9cfa0aea0a95796c579
Benjamin Boxler <benjaminboxler@gmail.com>
2014-04-14 00:28:58 +0100

    Added title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def title(page_title)
+    base_title = "Grit and Drew 2014"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
+
+
 end
