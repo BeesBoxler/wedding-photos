f27db8fabe163e262c46a65c154fe1a349054e37
Benjamin Boxler <benjaminboxler@gmail.com>
2014-05-25 02:35:09 +0100

    Auto rotate images

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -35,14 +35,14 @@ class ImageUploader < CarrierWave::Uploader::Base
   #   # do something
   # end
 
+  process :auto_orient
+
   # Create different versions of your uploaded files:
   version :thumb do
-    process :rotate
     process resize_to_fill: [250, 250]
   end
   
   version :large do
-    process :rotate
     process resize_to_fit: [1080, 1080]
   end
 
@@ -65,7 +65,7 @@ class ImageUploader < CarrierWave::Uploader::Base
       model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.hex(length/2))
     end
 
-    def rotate
+    def auto_orient
       manipulate! do |image|
         image.tap(&:auto_orient)
       end
