632d3d78d93068dfb43f2e8fcccfb55a2b0c05ba
Alex Wass <alexwass@btinternet.com>
2014-06-08 00:49:43 +0100

    Fixed IE8 display and added a warning popup

--- app/assets/javascripts/global.js
+++ app/assets/javascripts/global.js
@@ -8,6 +8,17 @@
 //	  console.log('DONE  - all images have been successfully loaded');
 //	});
 
+function readCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+
 $( document ).ready(function() {
 });
 
